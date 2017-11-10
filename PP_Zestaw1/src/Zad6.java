e5c1331ec74b8219d08ff3c890d54ef069a3339a
PAWEŁ <PAWE@192.168.1.71>
2017-11-10 15:04:54 +0100

    a

---
+++ PP_Zestaw1/src/Zad6.java
@@ -0,0 +1,23 @@
+/* Napisaæ program realizuj¹cy funkcje prostego kalkulatora, pozwalaj¹cego na wykonywanie operacji dodawania, 
+ * odejmowania, mno¿enia i dzielenia na dwóch liczbach rzeczywistych. Program ma identyfikowaæ sytuacjê
+ * wprowadzenia b³êdnego symbolu dzia³ania oraz próbê dzielenia przez zero. Zastosowaæ instrukcjê
+ * switch do wykonania odpowiedniego dzia³ania w zale¿noœci od wprowadzonego symbolu
+ * operacji. Scenariusz dzia³ania programu:
+ * a) Program wyœwietla informacjê o swoim przeznaczeniu.
+ * b) Wczytuje pierwsz¹ liczbê.
+ * c) Wczytuje symbol operacji arytmetycznej: +, -, *, /.
+ * d) Wczytuje drug¹ liczbê.
+ * e) Wyœwietla wynik lub - w razie koniecznoœci - informacjê o niemo¿noœci wykonania dzia³ania.
+ * f) Program koñczy swoje dzia³anie po naciœniêciu przez u¿ytkownika klawisza Enter
+*/
+package src;
+
+public class Zad6 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
